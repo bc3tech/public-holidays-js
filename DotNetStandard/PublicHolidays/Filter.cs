ef4442e6a8aaf666613602ea3b7f3fc9b0d8b78f
Brandon H <bc3tech@github.com>
2017-09-29 15:02:08 -0700

    modifying filter to be full-featured. in addition to specifying lang/country, take in start/end date as well as name.
    add XML doco for entire structure

--- DotNetStandard/PublicHolidays/Filter.cs
+++ DotNetStandard/PublicHolidays/Filter.cs
@@ -2,9 +2,23 @@
 
 namespace PublicHolidays
 {
+    /// <summary></summary>
     public class Filter
     {
-        public Filter(string country = @"us", string language = @"en")
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Filter"/> class.
+        /// </summary>
+        /// <param name="country">The country whose public holidays calendar should be loaded. Default: United States</param>
+        /// <param name="language">The language in which to load the calendar. Default: English</param>
+        /// <param name="startDate">The first date on which a public holiday may occur. Default: today</param>
+        /// <param name="endDate">The last date on which a public holiday may occur. Default: 1 year from today</param>
+        /// <param name="name">The name of the holiday to perform a case-insensitive substring search for.</param>
+        /// <exception cref="System.ArgumentNullException">
+        /// country
+        /// or
+        /// language
+        /// </exception>
+        public Filter(string country = @"us", string language = @"en", DateTime? startDate = null, DateTime? endDate = null, string name = null)
         {
             if (string.IsNullOrWhiteSpace(country)) throw new ArgumentNullException(nameof(country));
 
@@ -12,23 +26,34 @@ namespace PublicHolidays
 
             this.Country = country;
             this.Language = language;
+
+            this.StartDate = startDate ?? DateTime.Today;
+            this.EndDate = endDate ?? DateTime.Today.AddYears(1);
         }
 
-        public string Country { get; }
-        public string Language { get; }
+        /// <summary>
+        /// Gets or sets the country whose public holidays calendar should be loaded.
+        /// </summary>
+        public string Country { get; set; }
 
-        internal bool Matches(dynamic holiday, dynamic options)
-        {
-            if (options.start && holiday.start < options.start)
-            {
-                return false;
-            }
-            if (options.end && holiday.end > options.end)
-            {
-                return false;
-            }
+        /// <summary>
+        /// Gets or sets the language in which to load the calendar.
+        /// </summary>
+        public string Language { get; set; }
 
-            return true;
-        }
+        /// <summary>
+        /// Gets or sets the first date on which a public holiday may occur.
+        /// </summary>
+        public DateTime StartDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last date on which a public holiday may occur.
+        /// </summary>
+        public DateTime EndDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the holiday to search for.
+        /// </summary>
+        public string Name { get; set; }
     }
 }
